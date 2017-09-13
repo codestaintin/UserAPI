707368e0b16dc98472235e23f4dad54c2ea93a22
Isioye Mohammed <mohzak1103@gmail.com>
2017-09-13 04:42:30 -0700

    Code clean up

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,20 +1,13 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/users';
 
-// const excluded = ['/login', '/users'];
-
 const authenticate = (req, res, next) => {
-  // console.log("url ",req.url);
-  // if (excluded.indexOf(req.url) > -1) return next();
   const token = req.body.token || req.headers['x-access-token'];
   if (token) {
     jwt.verify(token, 'secretKey', (error, decoded) => {
       if (error) {
-        // console.error('JWT Verification Error', error);
         return res.status(403).json({ message: 'JWT Verification Error', error });
       }
-      // req.decoded = decoded;
-      // return next();
       User.findById(decoded.id)
         .then((user) => {
           if (!user) {
